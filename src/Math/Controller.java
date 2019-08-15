ae6ff2050883fd788e3c1f48a6ac04d0cad2f72b
Leo <leo.player@education.nsw.gov.au>
2019-08-15 22:46:51 +1000

    CSV Communication
    got csv to java communication, which opened many tasks

--- src/Math/Controller.java
+++ src/Math/Controller.java
@@ -11,9 +11,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.File;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static Math.Main.csvLanguageInitialise;
+
 /**
  * fxml made using Scene Builder 2
  * Controller class used to initialise floats, then update with field when submit pressed
@@ -35,7 +39,6 @@ public class Controller {
     private int adminLevel = 0; //0 not logged in, 1 logged in
     private int booted = 0; //1 booted, 0 not booted
     private int on = 0; //0 is off, 1 is on
-    private int language = 0; //0eng
     @FXML private TextArea mathOutput;
     @FXML private TextArea userInput;
     @FXML private Button plus;
@@ -44,7 +47,13 @@ public class Controller {
     @FXML private Button divide;
     @FXML private Button root;
     @FXML private Button memUn;
+    @FXML private Button memUnSet;
+    @FXML private Button memUnDelete;
     @FXML private Button memDeux;
+    @FXML private Button memDeuxSet;
+    @FXML private Button memDeuxDelete;
+    @FXML private Button c2mms;
+    @FXML private Button mms2c;
     @FXML private TextField controllerTitle;
     @FXML private TextField controllerOne;
     @FXML private TextField controllerTwo;
@@ -451,12 +460,12 @@ public class Controller {
         rootActive = 0;
     }
     private void inputError() {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Input Error");
-        alert.setHeaderText("Not a number");
-        alert.setContentText("Please put in a number");
+        Alert inputError = new Alert(Alert.AlertType.WARNING);
+        inputError.setTitle(Main.languageString[35]);
+        inputError.setHeaderText(Main.languageString[36]);
+        inputError.setContentText(Main.languageString[37]);
         LOGGER.log(Level.WARNING, "inputError");
-        alert.showAndWait();
+        inputError.showAndWait();
         focus();
     }
     private void focus() {
@@ -470,14 +479,21 @@ public class Controller {
         switch (moneySystemState) {
             case 0:
                 setOne();
-                on = 1;
                 break;
             case 1:
                 if (adminLevel == 0) setTwo();
                 else setThree();
                 break;
+            case 3:
+                setFour();
+                break;
+            case 4:
+                setLang1();
+                csvLanguageInitialise(new File("src/Math/CSV/english.csv"));
+                setLang2();
+                break;
             default:
-                LOGGER.log(Level.INFO, "I am bad at coding 1");
+                LOGGER.log(Level.INFO, "I am bad at coding 1: " + moneySystemState);
         }
         focus();
     }
@@ -487,17 +503,22 @@ public class Controller {
                 setTwenty();
                 break;
             default:
-                LOGGER.log(Level.INFO, "I am bad at coding 2");
+                LOGGER.log(Level.INFO, "I am bad at coding 2: " + moneySystemState);
         }
         focus();
     }
     @FXML protected void controllerThreeButton() {
         switch (moneySystemState) {
+            case 4:
+                setLang1();
+                csvLanguageInitialise(new File("src/Math/CSV/japanese.csv"));
+                setLang2();
+                break;
             case 10:
                 setThirty();
                 break;
             default:
-                LOGGER.log(Level.INFO, "I am bad at coding 3");
+                LOGGER.log(Level.INFO, "I am bad at coding 3: " + moneySystemState);
         }
         focus();
     }
@@ -507,7 +528,12 @@ public class Controller {
                 setTen();
                 break;
             case 2:
-                //check uname and password
+                if (!controllerThree.getText().equals("")&&controllerThree.getText().equals(
+                        superSecurePasswordDecoder(Main.superSecurePassword))) {
+                    LOGGER.info("admin pass success: " + controllerThree.getText());
+                    setThree();
+                }
+                adminLevel = 1;
                 break;
             case 3:
                 adminLevel = 0;
@@ -517,7 +543,7 @@ public class Controller {
                 setForty();
                 break;
             default:
-                LOGGER.log(Level.INFO, "I am bad at coding 4");
+                LOGGER.log(Level.INFO, "I am bad at coding 4: " + moneySystemState);
         }
         focus();
     }
@@ -532,6 +558,9 @@ public class Controller {
             case 3:
                 setOne();
                 break;
+            case 4:
+                setThree();
+                break;
             case 10:
                 setOne();
                 booted = 0;
@@ -542,9 +571,25 @@ public class Controller {
                 setTen();
                 break;
             default:
-                LOGGER.log(Level.INFO, "I am bad at coding 5");
+                LOGGER.log(Level.INFO, "I am bad at coding 5: " + moneySystemState);
         }
         focus();
+    }
+    private String superSecurePasswordDecoder(String hex){
+
+        StringBuilder sb = new StringBuilder();
+
+        //49204c6f7665204a617661 split into two characters 49, 20, 4c...
+        for( int i=0; i<hex.length()-1; i+=2 ){
+
+            //grab the hex in pairs
+            String output = hex.substring(i, (i + 2));
+            //convert hex to decimal
+            int decimal = Integer.parseInt(output, 16);
+            //convert the decimal to character
+            sb.append((char)decimal);
+        }
+        return sb.toString();
     }
     private void setOne() { //boot screen
         clearControllers();
@@ -592,72 +637,82 @@ public class Controller {
         }
     }
     private void actuallySetOne() {
-        controllerTitle.setText("Money Management System");
+        on = 1;
+        controllerTitle.setText(Main.languageString[0]);
         controllerOne.setStyle("-fx-alignment: CENTER");
         Random rand = new Random();
         int randomMessage = rand.nextInt(10);
         switch (randomMessage) {
             case 0:
-                controllerOne.setText("Approved by Jaden!");
+                controllerOne.setText(Main.languageString[1]);
                 break;
             case 1:
-                controllerOne.setText("\"France is just the northern part of Australia!\" - Fish, 2019");
+                controllerOne.setText(Main.languageString[2]);
                 break;
             case 2:
-                controllerOne.setText("something creative 2");
+                controllerOne.setText(Main.languageString[3]);
                 break;
             case 3:
-                controllerOne.setText("something creative 3");
+                controllerOne.setText(Main.languageString[4]);
                 break;
             case 4:
-                controllerOne.setText("something creative 4");
+                controllerOne.setText(Main.languageString[5]);
                 break;
             case 5:
-                controllerOne.setText("something creative 5");
+                controllerOne.setText(Main.languageString[6]);
                 break;
             case 6:
-                controllerOne.setText("something creative 6");
+                controllerOne.setText(Main.languageString[7]);
                 break;
             case 7:
-                controllerOne.setText("something creative 7");
+                controllerOne.setText(Main.languageString[8]);
                 break;
             case 8:
-                controllerOne.setText("something creative 8");
+                controllerOne.setText(Main.languageString[9]);
                 break;
             case 9:
-                controllerOne.setText("something creative 9");
+                controllerOne.setText(Main.languageString[10]);
                 break;
             default:
                 LOGGER.log(Level.SEVERE, "error comment");
                 System.exit(11);
         }
-        controllerFour.setText("Boot");
-        controllerFive.setText("Off");
+        controllerFour.setText(Main.languageString[11]);
+        controllerFive.setText(Main.languageString[12]);
         moneySystemState = 1;
-        //focus(); dunno why but fails in MMS
     }
     private void setTwo() { //admin login
         clearControllers();
-        controllerTitle.setText("Admin");
+        controllerTitle.setText(Main.languageString[13]);
         controllerOne.setStyle("-fx-alignment: CENTER");
-        controllerOne.setText("Input for access");
-        controllerTwo.setPromptText("User Name");
+        controllerOne.setText(Main.languageString[14]);
+        controllerTwo.setPromptText(Main.languageString[15]);
         controllerTwo.setEditable(true);
-        controllerThree.setPromptText("Password");
+        controllerThree.setPromptText(Main.languageString[16]);
         controllerThree.setEditable(true);
-        controllerFour.setText("Enter");
-        controllerFive.setText("Back");
+        controllerFour.setText(Main.languageString[17]);
+        controllerFive.setText(Main.languageString[18]);
         moneySystemState = 2;
     }
     private void setThree() { //admin screen
         clearControllers();
-        controllerTitle.setText("Admin Controls");
-        controllerOne.setText("");
-        controllerTwo.setText("");
-        controllerThree.setText("");
-        controllerFour.setText("Logout");
-        controllerFive.setText("Back Without Logout");
+        controllerTitle.setText(Main.languageString[19]);
+        controllerOne.setText(Main.languageString[20]);
+        controllerTwo.setText(Main.languageString[21]);
+        controllerThree.setText(Main.languageString[22]);
+        controllerFour.setText(Main.languageString[23]);
+        controllerFive.setText(Main.languageString[24]);
         moneySystemState = 3;
+    }
+    private void setFour() {
+        clearControllers();
+        controllerTitle.setText(Main.languageString[38]);
+        controllerOne.setText("English");
+        controllerTwo.setText("Français");
+        controllerThree.setText("日本語");
+        controllerFour.setText("Italiano");
+        controllerFive.setText(Main.languageString[18]);
+        moneySystemState = 4;
     }
     private void setTen() { //main screen
         clearControllers();
@@ -667,35 +722,35 @@ public class Controller {
                         @Override
                         public void run() {
                             controllerOne.setStyle("-fx-alignment: CENTER");
-                            controllerOne.setText("Money Management System");
+                            controllerOne.setText(Main.languageString[0]);
                             new java.util.Timer().schedule(
                                     new java.util.TimerTask() {
                                         @Override
                                         public void run() {
                                             clearControllers();
                                             controllerTwo.setStyle("-fx-alignment: CENTER");
-                                            controllerTwo.setText("Money Management System");
+                                            controllerTwo.setText(Main.languageString[0]);
                                             new java.util.Timer().schedule(
                                                     new java.util.TimerTask() {
                                                         @Override
                                                         public void run() {
                                                             clearControllers();
                                                             controllerThree.setStyle("-fx-alignment: CENTER");
-                                                            controllerThree.setText("Money Management System");
+                                                            controllerThree.setText(Main.languageString[0]);
                                                             new java.util.Timer().schedule(
                                                                     new java.util.TimerTask() {
                                                                         @Override
                                                                         public void run() {
                                                                             clearControllers();
                                                                             controllerFour.setStyle("-fx-alignment: CENTER");
-                                                                            controllerFour.setText("Money Management System");
+                                                                            controllerFour.setText(Main.languageString[0]);
                                                                             new java.util.Timer().schedule(
                                                                                     new java.util.TimerTask() {
                                                                                         @Override
                                                                                         public void run() {
                                                                                             clearControllers();
                                                                                             controllerFive.setStyle("-fx-alignment: CENTER");
-                                                                                            controllerFive.setText("Money Management System");
+                                                                                            controllerFive.setText(Main.languageString[0]);
                                                                                             new java.util.Timer().schedule(
                                                                                                     new java.util.TimerTask() {
                                                                                                         @Override
@@ -741,49 +796,48 @@ public class Controller {
     }
     private void actuallySetTen() {
         clearControllers();
-        controllerTitle.setText("Main Menu");
-        //controllerOne.setText(""); to put something else in
-        controllerTwo.setText("Check Balance");
-        controllerThree.setText("Add to Balance");
-        controllerFour.setText("Subtract from Balance");
-        controllerFive.setText("Exit");
+        controllerTitle.setText(Main.languageString[25]);
+        controllerTwo.setText(Main.languageString[26]);
+        controllerThree.setText(Main.languageString[27]);
+        controllerFour.setText(Main.languageString[28]);
+        controllerFive.setText(Main.languageString[29]);
         moneySystemState = 10;
         booted = 1;
     }
     private void setTwenty() { //check balance
         clearControllers();
-        controllerTitle.setText("Check Balance");
+        controllerTitle.setText(Main.languageString[26]);
         controllerOne.setStyle("-fx-alignment: CENTER");
-        controllerOne.setText("Balance");
+        controllerOne.setText(Main.languageString[30]);
         //balance
         controllerThree.setStyle("-fx-alignment: CENTER");
-        controllerThree.setText("Your Balance");
+        controllerThree.setText(Main.languageString[31]);
         //users balance
-        controllerFive.setText("Back");
+        controllerFive.setText(Main.languageString[18]);
         moneySystemState = 20;
     }
     private void setThirty() { //add
         clearControllers();
-        controllerTitle.setText("Add to Balance");
+        controllerTitle.setText(Main.languageString[27]);
         controllerOne.setStyle("-fx-alignment: CENTER");
-        controllerOne.setText("Current Balance");
+        controllerOne.setText(Main.languageString[32]);
         //balance
-        controllerThree.setPromptText("Amount to add");
+        controllerThree.setPromptText(Main.languageString[33]);
         controllerThree.setEditable(true);
-        controllerFour.setText("Enter");
-        controllerFive.setText("Back");
+        controllerFour.setText(Main.languageString[17]);
+        controllerFive.setText(Main.languageString[18]);
         moneySystemState = 30;
     }
     private void setForty() { //subtract
         clearControllers();
-        controllerTitle.setText("Subtract from Balance");
+        controllerTitle.setText(Main.languageString[28]);
         controllerOne.setStyle("-fx-alignment: CENTER");
-        controllerOne.setText("Current Balance");
+        controllerOne.setText(Main.languageString[32]);
         //balance
-        controllerThree.setPromptText("Amount to subtract");
+        controllerThree.setPromptText(Main.languageString[34]);
         controllerThree.setEditable(true);
-        controllerFour.setText("Enter");
-        controllerFive.setText("Back");
+        controllerFour.setText(Main.languageString[17]);
+        controllerFive.setText(Main.languageString[18]);
         moneySystemState = 40;
     }
     private void clearControllers() {
@@ -798,9 +852,25 @@ public class Controller {
             textField.setPromptText(null);
         }
     }
-
-    //Language setting
-    //change prompt text, text display and c2mms/mms2c button text
+    private void setLang1() {
+        on = 0;
+        moneySystemState = 0;
+        clearControllers();
+    }
+    private void setLang2() {
+        userInput.setPromptText(Main.languageString[39]);
+        mathOutput.setPromptText(Main.languageString[40]);
+        memUn.setText(Main.languageString[41]);
+        memUnSet.setText(Main.languageString[42]);
+        memUnDelete.setText(Main.languageString[43]);
+        memDeux.setText(Main.languageString[44]);
+        memDeuxSet.setText(Main.languageString[42]);
+        memDeuxDelete.setText(Main.languageString[43]);
+        c2mms.setText(Main.languageString[45]);
+        mms2c.setText(Main.languageString[46]);
+        LOGGER.config("Language Initialised");
+        setOne();
+    }
 
     //communication between systems
 
--- src/Math/Main.java
+++ src/Math/Main.java
@@ -5,7 +5,6 @@
 
 package Math;
 
-import com.opencsv.CSVReader;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -15,8 +14,11 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.logging.*;
 
@@ -26,6 +28,12 @@ public class Main extends Application {
      * PrintStream section changes system output to file on boot
      * setOnCloseRequest runs confirmation of closing application
      */
+    static String[] languageString;
+    static String superSecurePassword;
+    private int lf = 0;
+    private int csvfp = 0;
+    private String lfMessage;
+    private String csvfpMessage;
 
     @Override
     public void init() {
@@ -46,37 +54,27 @@ public class Main extends Application {
             fileHandler.setLevel(Level.ALL);
             LOGGER.setLevel(Level.ALL);
 
-            LOGGER.config("Configuration done.");
+            LOGGER.config("Logger Configuration done.");
 
             //Console handler removed
             LOGGER.removeHandler(consoleHandler);
 
         } catch (IOException exception) {
             LOGGER.log(Level.SEVERE, "FAILED TO CREATE LOG", exception);
-            System.exit(1);
+            lf = 1;
+            lfMessage = String.valueOf(exception);
         }
-        File language = new File("src/Math/CSV/languages.csv");
-        BufferedReader br;
-        String line;
-        String cvsSplitBy = ",";
+        csvLanguageInitialise(new File("src/Math/CSV/english.csv"));
         try {
-            br = new BufferedReader(new FileReader(language));
-            while ((line = br.readLine()) != null) {
-                // use comma as separator
-                String[] languageString = line.split(cvsSplitBy);
-
-                System.out.println(languageString[0]);
-
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            LOGGER.log(Level.SEVERE, "Failed CSV 1: " + e.getStackTrace() + " " + e.getCause());
-            System.exit(2);
+            BufferedReader br = new BufferedReader(new FileReader(new File("src/Math/CSV/password.csv")));
+            superSecurePassword = br.readLine().substring(1,17); //dunno why but readline adds space at start and end
         } catch (IOException e) {
             e.printStackTrace();
-            LOGGER.log(Level.SEVERE, "Failed CSV 2: " + e.getStackTrace() + " " + e.getCause());
-            System.exit(3);
+            LOGGER.log(Level.SEVERE, "Failed CSV password: " + Arrays.toString(e.getStackTrace()));
+            csvfp = 1;
+            csvfpMessage = Arrays.toString(e.getStackTrace());
         }
+        LOGGER.config("Configuration complete.");
     }
 
     @Override
@@ -86,14 +84,19 @@ public class Main extends Application {
         primaryStage.setTitle("Adding Two Numbers");
         primaryStage.setScene(new Scene(root, 860, 600));
         primaryStage.show();
+        if (lf == 1) {
+            logFailAlert();
+        } else if (csvfp == 1) {
+            csvFailPasswordAlert();
+        }
 
         primaryStage.setOnCloseRequest(event -> {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Quitting Application");
-            alert.setHeaderText("Are you sure you want to close this incredible application?");
-            alert.setContentText("You will regret it");
+            Alert closing = new Alert(Alert.AlertType.CONFIRMATION);
+            closing.setTitle("Quitting Application");
+            closing.setHeaderText("Are you sure you want to close this incredible application?");
+            closing.setContentText("You will regret it");
 
-            Optional<ButtonType> result = alert.showAndWait();
+            Optional<ButtonType> result = closing.showAndWait();
             if (result.isPresent()) {
                 if (result.get() == ButtonType.OK) {
                     LOGGER.log(Level.CONFIG, "User closed program");
@@ -104,5 +107,59 @@ public class Main extends Application {
                 }
             }
         });
+    }
+
+    private void logFailAlert() {
+        final Logger LOGGER = Logger.getLogger(Main.class.getName());
+        Alert logFail = new Alert(Alert.AlertType.CONFIRMATION);
+        logFail.setTitle("Log Creation Failed");
+        logFail.setHeaderText("Do you want to continue program?");
+        logFail.setContentText(lfMessage);
+
+        Optional<ButtonType> result = logFail.showAndWait();
+        if (result.isPresent()) {
+            if (result.get() == ButtonType.OK) {
+                LOGGER.log(Level.CONFIG, "User continued program, LogFail");
+            } else {
+                LOGGER.log(Level.CONFIG, "User terminated program, LogFail");
+                System.exit(1);
+            }
+        }
+    }
+    private void csvFailPasswordAlert() {
+        final Logger LOGGER = Logger.getLogger(Main.class.getName());
+        Alert csvFailPass = new Alert(Alert.AlertType.CONFIRMATION);
+        csvFailPass.setTitle("Password load failed");
+        csvFailPass.setHeaderText("Do you want to continue program?");
+        csvFailPass.setContentText(csvfpMessage);
+
+        Optional<ButtonType> result = csvFailPass.showAndWait();
+        if (result.isPresent()) {
+            if (result.get() == ButtonType.OK) {
+                LOGGER.log(Level.CONFIG, "User continued program, Pass fail");
+            } else {
+                LOGGER.log(Level.CONFIG, "User terminated program, Pass fail");
+                System.exit(2);
+            }
+        }
+    }
+    static void csvLanguageInitialise(File csvs) {
+        final Logger LOGGER = Logger.getLogger(Main.class.getName());
+        File language = new File(String.valueOf(csvs));
+        BufferedReader br;
+        String line;
+        String cvsSplitBy = ",";
+        try {
+            br = new BufferedReader(new FileReader(language));
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                languageString = line.split(cvsSplitBy);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Failed CSV: " + Arrays.toString(e.getStackTrace()) + " csv: " + csvs);
+            System.exit(3);
+        }
+        LOGGER.config("CSV Configuration done.");
     }
 }
